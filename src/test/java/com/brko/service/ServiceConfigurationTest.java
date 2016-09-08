21ddf3f39fb90086c2312e97bcb2b2f927744a84
Petre <petrepp4@gmail.com>
2016-09-08 15:45:32 +0200

    Added integration tests configuration.

--- src/test/java/com/brko/service/ServiceConfigurationTest.java
+++ src/test/java/com/brko/service/ServiceConfigurationTest.java
@@ -1,19 +1,18 @@
-package com.brko.service.crawler.impl;
+package com.brko.service;
 
-import com.brko.config.BrkoTestConfiguration;
 import com.brko.service.crawler.exceptions.ReadPaperSummaryFromHtmlException;
 import org.jsoup.HttpStatusException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
+ * Test class for Spring configuration.
+ *
  * Created by Petre on 9/5/2016.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = BrkoTestConfiguration.class)
-public class ServiceConfigurationTest {
+public class ServiceConfigurationTest extends AbstractServiceIntegrationTest{
 
     @Test
     public void testConfiguration() throws HttpStatusException, ReadPaperSummaryFromHtmlException {
